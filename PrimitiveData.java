5ba39d46206675fd18d80a7265dd895db42701d7
srushtik2 <srushtixworkz@gmail.com>
2021-10-26 22:12:35 +0530

    DataTypes

---
+++ PrimitiveData.java
@@ -0,0 +1,34 @@
+class PrimitiveData{
+	public static void main (String[] sru){
+		
+		int marks = 526;
+		System.out.println(marks);
+		
+		byte number = 127;
+		System.out.println(number);
+		
+		short price = 132;
+        System.out.println(price);
+		
+		double num = -555.23d;
+		System.out.println(num);
+		
+        float cgpa = 6.33f;
+        System.out.println(cgpa);
+        
+        long phonenum = 9916144877L;
+        System.out.println(phonenum);
+		
+		char c = 's';
+		System.out.println(c);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+	}
+	
+}
+		
+		
+      
+        	  
