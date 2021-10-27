6707a1d9b1462751cd39226e5ac3bd3bb6089e7e
srushtik2 <srushtixworkz@gmail.com>
2021-10-27 21:54:42 +0530

    ConditionalStatements

---
+++ WashingMachine.java
@@ -0,0 +1,25 @@
+class WashingMachine
+{
+	public static void main(String []sru)
+	{
+		boolean powder = false;
+		
+		System.out.println("Starting Washing Cloths");
+		System.out.println("Adding Cloths");
+		System.out.println("Adding Washing Powder");
+		
+		if (powder == true)
+		{
+			
+		System.out.println("Start Washing Machine");
+		}
+		else
+		{
+			System.out.println("Start Washing Cloths Manually");
+		}
+		
+		System.out.println("Cloths Washed");
+	}
+}
+		
+		
