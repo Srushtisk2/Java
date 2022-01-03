0b60854e840602e9a707e86408e58859c93797ae
srushtik2 <srushtixworkz@gmail.com>
2022-01-03 10:35:51 +0530

    Date_Time

---
+++ DateTime.java
@@ -0,0 +1,11 @@
+package com.xworkz.date_time;
+	
+	import java.time.LocalDateTime; 
+	public class DateTime {
+	  public static void main(String[] args) {
+	    LocalDateTime datetime = LocalDateTime.now();
+	    System.out.println(datetime);
+	  }
+	}
+
+
