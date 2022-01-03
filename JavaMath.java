871a6a89dade7daa8c0550932ece9a779d73ccf5
srushtik2 <srushtixworkz@gmail.com>
2022-01-03 10:38:52 +0530

    Math_Operator

---
+++ JavaMath.java
@@ -0,0 +1,22 @@
+package com.xworkz.math;
+
+public class JavaMath 
+{    
+    public static void main(String[] args)     
+    {    
+        double x = 28;    
+        double y = 4;    
+            
+        System.out.println("Maximum number of x and y is: " +Math.max(x, y));   
+            
+        System.out.println("Square root of y is: " + Math.sqrt(y));   
+               
+        System.out.println("log10 of x is: " + Math.log10(x)); 
+        
+        System.out.println("log10 of y is: " + Math.log10(y));    
+          
+        
+       
+        
+    }    
+}    
